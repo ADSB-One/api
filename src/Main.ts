70453e3f044b54ec7cf3fe4662b9e315e0785021
Husky <22480762+Huusky@users.noreply.github.com>
2023-09-03 14:18:08 -0400

    Sync with running code

--- src/Main.ts
+++ src/Main.ts
@@ -26,7 +26,6 @@ server.get('/v2/callsign/:callsign', async (req: any, res) => {
     var callsign = req.params.callsign;
     
     var callsignRes: any = await axios.get(`http://${readsb_api_host}:${readsb_api_port}/re-api/?find_callsign=${callsign}&jv2`);
-    callsignRes = JSON.stringify(callsignRes.data);
     
     res.type('json');
     res.send(callsignRes);
@@ -37,7 +36,6 @@ server.get('/v2/reg/:reg', async (req: any, res) => {
     var reg = req.params.reg;
     
     var regRes: any = await axios.get(`http://${readsb_api_host}:${readsb_api_port}/re-api/?find_reg=${reg}&jv2`);
-    regRes = JSON.stringify(regRes.data);
     
     res.type('json');
     res.send(regRes);
@@ -48,7 +46,6 @@ server.get('/v2/type/:type', async (req: any, res) => {
     var type = req.params.type;
     
     var typeRes: any = await axios.get(`http://${readsb_api_host}:${readsb_api_port}/re-api/?find_type=${type}&jv2`);
-    typeRes = JSON.stringify(typeRes.data);
     
     res.type('json');
     res.send(typeRes);
@@ -59,7 +56,6 @@ server.get('/v2/squawk/:squawk', async (req: any, res) => {
     var squawk = req.params.squawk;
     
     var squawkRes: any = await axios.get(`http://${readsb_api_host}:${readsb_api_port}/re-api/?all&filter_squawk=${squawk}&jv2`);
-    squawkRes = JSON.stringify(squawkRes.data);
     
     res.type('json');
     res.send(squawkRes);
@@ -68,7 +64,6 @@ server.get('/v2/squawk/:squawk', async (req: any, res) => {
 // Get all currently tracked military aircraft
 server.get('/v2/mil/', async (_req: any, res) => {
     var milRes: any = await axios.get(`http://${readsb_api_host}:${readsb_api_port}/re-api/?all&filter_mil&jv2`);
-    milRes = JSON.stringify(milRes.data);
     
     res.type('json');
     res.send(milRes);
@@ -77,7 +72,6 @@ server.get('/v2/mil/', async (_req: any, res) => {
 // Get all currently tracked LADD aircraft
 server.get('/v2/ladd/', async (_req: any, res) => {
     var laddRes: any = await axios.get(`http://${readsb_api_host}:${readsb_api_port}/re-api/?all&filter_ladd&jv2`);
-    laddRes = JSON.stringify(laddRes.data);
     
     res.type('json');
     res.send(laddRes);
@@ -86,7 +80,6 @@ server.get('/v2/ladd/', async (_req: any, res) => {
 // Get all currently tracked PIA aircraft
 server.get('/v2/pia/', async (_req: any, res) => {
     var piaRes: any = await axios.get(`http://${readsb_api_host}:${readsb_api_port}/re-api/?all&filter_pia&jv2`);
-    piaRes = JSON.stringify(piaRes.data);
     
     res.type('json');
     res.send(piaRes);
@@ -100,7 +93,6 @@ server.get('/v2/point/:lat/:lon/:rad', async (req: any, res) => {
     rad > 250 ? rad = 250 : rad = rad;
 
     var pointRes: any = await axios.get(`http://${readsb_api_host}:${readsb_api_port}/re-api/?circle=${lat},${lon},${rad}&jv2`);
-    pointRes = JSON.stringify(pointRes.data);
     
     res.type('json');
     res.send(pointRes);
